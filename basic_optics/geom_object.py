3c940931bac483253219afd121c05e7574023dcc
Clemens <clemens.anschuetz@uni-jena.de>
2023-03-28 17:44:07 +0200

    Begin Documentation Geom_Object
    bis jetzt nur auf Deutsch, aber jede Funktion hat ihren Docstring. Außerdem nimmt angle_to() jetzt ein obj mit normal an und keinen vector mehr

--- basic_optics/geom_object.py
+++ basic_optics/geom_object.py
@@ -5,11 +5,8 @@ Created on Thu Aug 18 10:46:05 2022
 @author: mens
 """
 
-# print("geom_object modul wird importiert")
-
 import numpy as np
-from scipy.spatial.transform import Rotation as Rot
-from basic_optics.freecad_models import model_lens, freecad_da
+from basic_optics.freecad_models import freecad_da
 
 
 NAME0="unnamed"
@@ -17,17 +14,14 @@ POS0 = np.array((0,0,80)) #Strahlhöhe 80 mm
 NORM0 = np.array((1,0,0)) #Strahl startet in x-Richtung
 TOLERANCE = 1e-8 #Wert ab dem zwei Größen (meist Winkel) als gleich angenommen werden
 
-# def is_round_about(): #sollte mal toleranz einarbeiten, wurde aber noch nicht geschrieben
-# 	return False
 
-# def rotation_matrix(v, phi):
-#     """berechnet die Rotationsmatrix R_v(phi) um den Vector v im R^3"""
-#     v = np.array(v)
-#     v /= np.linalg.norm(v)  # falls v nicht normiert ist
-#     return Rot.from_rotvec(phi * v).as_matrix()
 
 def rotation_matrix(vec, phi):
-	"""berechnet die Rotationsmatrix R_v(phi) um den Vectro v im R^3"""
+	"""
+  berechnet die Rotationsmatrix R_v(phi) um den Vektor <vec> im R^3
+  
+  calculates the rotation matrix R_v(phi) around the vector <vec> in R^3
+  """
 	vec = np.array(vec)*1.0 #erst mal in ein float array
 	v_abs = np.linalg.norm(vec)
 	vec *= 1/v_abs
@@ -44,7 +38,13 @@ def rotation_matrix(vec, phi):
 	return rot_mat
 
 def rotation_matrix_from_vectors(vec1, vec2):
-  """ Find the rotation matrix that aligns vec1 to vec2
+  """ 
+  Findet die Rotationsmatrix, die vec1 zu vec2 ausrichtet
+  :param vec1: Ein 3D-"Quellen"-Vektor
+  :param vec2: Ein 3D-"Ziel"-Vektor
+  :return mat: Eine Transformationsmatrix (3x3), die, wenn sie auf vec1 angewendet wird, sie mit vec2 ausrichtet.
+   
+  Finds the rotation matrix that aligns vec1 to vec2
   :param vec1: A 3d "source" vector
   :param vec2: A 3d "destination" vector
   :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
@@ -53,7 +53,8 @@ def rotation_matrix_from_vectors(vec1, vec2):
   vec2 = np.array(vec2) *1.0
   vec1 *= 1/np.linalg.norm(vec1)
   vec2 *= 1/np.linalg.norm(vec2)
-  a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
+  a = (vec1 / np.linalg.norm(vec1)).reshape(3)
+  b =  (vec2 / np.linalg.norm(vec2)).reshape(3)
   v = np.cross(a, b)
   if any(v): #if not all zeros then
     c = np.dot(a, b)
@@ -64,7 +65,6 @@ def rotation_matrix_from_vectors(vec1, vec2):
       return np.eye(3) #cross of all zeros only occurs on identical directions
 
 
-
 def vec2str(vec):
 	return repr(np.round(vec, decimals=5))[6:-1]
 
@@ -73,31 +73,34 @@ def vec2str(vec):
 
 class Geom_Object(object):
   """
-  speichert die elementaren Informationen über Position und Ausrichtung
-  und Namen, definiert die Defaults für alle folgenden Objekte
-  passt bei Änderung der normal automatisch das inner Koordiantwensystem an
-  Bsp: geom = Geom_Object(name="ErsterPunkt", pos=(1,2,3), norm=(-1,2,0))
+  speichert die elementaren Informationen über Position <pos>, Ausrichtung 
+  <normal> und Namen <name>, 
+  Definiert die Defaults für alle folgenden Objekte, passt bei Änderung der 
+  <normal> automatisch das innere Koordiantwensystem <_axes> an und ruft dann 
+  die Hilfsfunktionen <_pos_changed> und <_axes_changed> auf
+  
+  führt zur Darstellung das <draw_dict> mit allen wichtigen keyword arguments 
+  ein, kann beliebig erweitert werden, wird an die draw_fc Routinen weiter
+  gegeben
+  
   siehe tests()
-  Methoden:
-    siehe __dict__
-
-
   """
   def __init__(self, name=NAME0, pos=POS0, normal=NORM0):
     self.name = name
     self._pos = pos
-    # self._normal = normal # wird schon später noch gebraucht
     self._axes = np.eye(3)
-    # self.__normal = normal/ np.linalg.norm(normal) #als property immer auf 1 normiert
     # das eigene Kordinatensystem, die erste Spalte ist immer die Normale
     self._axes = self._updated_axes(normal, NORM0)
-    # self.normal = self.__normal #um das axes zu ändern und alle Folgefunktionen aufzurufen falls nötig
     self.draw_dict = {"name": self.name, "geom":self.get_geom()} #für die Cosmetics
 
-  # stellt sicher, das immer Copies von norm und pos übergeben werden
-  # und alles float arrays sind, normiert die normal, updatet das eigene Koo-Sys "axes"
+
   @property
   def pos(self):
+    """
+    beschreibt die Position des GeomObject als 3D-numpy-float-array
+    stellt über Setter und Getter sicher, dass nur Kopien übergeben werden und
+    das _pos_changed aufgrufen wird
+    """
     return np.array(self._pos) * 1.0
   @pos.setter
   def pos(self, x):
@@ -107,31 +110,44 @@ class Geom_Object(object):
 
   @property
   def normal(self):
+    """
+    Beschreibt die Ausrichtung (= interne x-Achse) des GeomObject als 3D-numpy-
+    float-array mit Betrag 1
+    stellt über Setter und Getter sicher, dass nur Kopien übergeben werden, der 
+    Vektor stehts normiert wird und das die _axes aktualisiert wird
+    """
     return np.array(self._axes[:,0])
   @normal.setter
   def normal(self, x):
-    # old_normal = np.array(self.__normal)
     old_normal = self.normal
-    # self.__normal = x / np.linalg.norm(x)
     new_normal = x / np.linalg.norm(x)
-    # print(new_normal, old_normal)
-    # self.__normal_changed(old__normal, x)
     self.set_axes(self._updated_axes(new_normal, old_normal))
-    # self._axes[:,0] = np.array(self.__normal)
+
 
   def get_axes(self):
-    # gibt _axes zurück
+    """
+    gibt das eigene Kooridnatensystem _axes als 3x3 Matrix zurück
+    indem die x,y,z-Vektoren als Spaltenvektoren stehen
+    """
     return np.array(self._axes)
   
   def get_coordinate_system(self):
-    # gibt normal, senkrecht1 und senkrecht2 als 3 Vektoren zurück
-    m = np.array(self._axes)
-    return m[:,0], m[:,1], m[:,2]
+    """
+    gibt normal (x), senkrecht1 (y) und senkrecht2 (z) als 3 Vektoren zurück
+    (transponiert zu self.get_axes)
+    """
+    mat = np.array(self._axes)
+    return mat[:,0], mat[:,1], mat[:,2]
 
   def set_axes(self, new_axes):
+    """
+    setzt das eigene Koordinatensystem auf <new_axes> und ruft 
+    _axes_changed auf
+
+    new_axes : reelle, orthogonale 3x3 numpy float Matrix mit Determinante +1
+    """
     old_axes = self.get_axes()
     self._axes = np.array(new_axes)
-    # self.__normal = np.array(new_axes[:,0])
     self._axes_changed(old_axes, new_axes)
     
 
@@ -139,45 +155,31 @@ class Geom_Object(object):
     """
     old_normal : 3D-array
     berechnet das neue Koordinatensystem durch Drehmatrix zwischen
-    (neu-) normal und old_normal
+    (neu-) normal und old_normal (setzt es aber nicht)
     """
-    # print("norm, oldnorm:", (self.__normal, old_normal))
-    v = np.cross(new_normal, old_normal) # Drehvektor
-    v_abs = np.linalg.norm(v)
-    # if v_abs > 1:
-      # print("---",v_abs,"---")
-      # v_abs = 1
-    # elif v_abs < -1:
-      # print("---",v_abs,"---")
-      # v_abs = -1
-    # phi = np.arcsin(v_abs)
+    rotvec = np.cross(new_normal, old_normal) # Drehvektor
+    v_abs = np.linalg.norm(rotvec)
     skalarP = np.sum(new_normal * old_normal)
     if v_abs < TOLERANCE:
       if skalarP >= 0:
         # phi kleiner als 1e-8
-        # print("GO: phi kleiner als 1e-8")
         return self._axes
       else:
         # phi rund 180°, d.h. die drehung ist nicht eindeutig, negieren
         # willkürlich ax0 und ax1 und behalten ax2 bei um rechtshändiges KooSys
         # zu erhalten. (ax0 und ax2 würden genau so funktionieren)
-        # print("GO:phi rund 180°")
         a,b,c = self.get_coordinate_system()
         a = new_normal #just in case
         return np.vstack((a,-b,c))
     else:
-      # rot_mat = rotation_matrix(v, phi)
       rot_mat = rotation_matrix_from_vectors(old_normal, new_normal)
-      # print("axes:",  self._axes)
-      # print("rotmat", rot_mat)
-      # print("result", np.matmul(rot_mat, self._axes))
       return np.matmul(rot_mat, self._axes)
 
 
-  def angle_to(self, vec):
-    """computes the angle from a "z-height-ray to the Vector vec with
+  def angle_to(self, obj):
+    """computes the angle to the normal of an other GeomObj <obj> with
     correct sign"""
-    vec = vec / np.linalg.norm(vec)
+    vec = obj.normal
     c = np.cross(self.normal, vec)
     sign = 1 if np.sum(c * self._axes[:,1]) >= 0 else -1
     return np.arcsin(np.linalg.norm(c))*sign
@@ -196,13 +198,11 @@ class Geom_Object(object):
     """
     rot_mat = rotation_matrix(vec, -phi)
     new_ax = np.matmul(rot_mat, self._axes)
-    # self.__normal = new_ax[:,0]
-    self.set_axes(new_ax ) #nur um sicher zu gehen, kP, Drehungen im R3 halt
+    self.set_axes(new_ax) 
 
 
   def __repr__(self):
-#     txt = 'Geom_Object(name="' + self.name
-    txt = self.Klassenname()+ '(name="' + self.name
+    txt = self.class_name()+ '(name="' + self.name
     txt += '", pos='+vec2str(self.pos)
     txt += ", normal="+vec2str(self.normal)+")"
     return txt
@@ -210,10 +210,14 @@ class Geom_Object(object):
   def __str__(self):
     return ">>> " + repr(self)
 
-  def Klassenname(cls):
+  def class_name(cls):
     return str(type(cls)).split(".")[-1].split("'")[0]
 
   def get_geom(self):
+    """
+    gibt das so genannte geom zurück, einfach nur ein Tupel aus <pos> und 
+    <normal>, wichtig zur geometrischen Definition der meisten Objekte
+    """
     return (self.pos, self.normal)
 
   def set_geom(self, geom):
@@ -221,10 +225,20 @@ class Geom_Object(object):
     self.normal = np.array(geom[1])
     
   def update_draw_dict(self):
+    """
+    wird meist for den draw_routinen aufgerufen und kann die wichtigesten 
+    key word arguments des <draw_dict> aktualisieren, falls sie genändert 
+    wurden (z.B. self.radius von Curved_Mirror)
+    """
     self.draw_dict["name"]=self.name
     self.draw_dict["geom"]=self.get_geom() 
 
   def draw(self):
+    """
+    Funktion zur Darstellung des Objekts
+    prüft nach, ob freecad als Backend verfügbar ist und ruft dann die 
+    entsprechende draw-Funktion auf
+    """
     if freecad_da:
       return self.draw_fc()
     else:
@@ -233,35 +247,26 @@ class Geom_Object(object):
       return txt
 
   def draw_fc(self):
+    """
+    ruft falls FreeCAD vorhanden ist die entsprechende Zeichenfunktion aus
+    freecad_models auf, gibt im Normalfall eine Referenz auf das entsprechende
+    FreeCAD-Objekt zurück
+    """
     #ToDo: fürs Debugging hier einfach einen Zylinder mit norm uns k zeichnen
     return None
 
   def draw_text(self):
+    """
+    falls FreeCAD nicht zur Verfügung steht, wird das Objekt in die Konsole 
+    "gezeichnet", d.h. alle relevanten Parameter aus dem <draw_dict> werden
+    per Text ausgegeben, gibt den Text als str zurück
+    """
 #     txt = "Das geometrische Objekt <" + self.name + "> wird an die Position "
-    txt = "Das Objekt <" + self.Klassenname() + ":" +self.name
+    txt = "Das Objekt <" + self.class_name() + ":" +self.name
     txt += "> wird an die Position "
     txt += vec2str(self.pos) + " mit der Ausrichtung " + vec2str(self.normal)
     txt += " gezeichnet."
     return txt
-
-  # def FreeCAD_model(self):
-  #   """
-  #   deprecated
-  #   """
-  #   model_lens()
-
-  def is_equal(self, obj):
-    """
-    checkt ob <self> und obj äquivalent sind, also ob sie die gleichen Einträge
-    haben
-    returns True/False
-    """
-    d1 = self.__dict__
-    d2 = obj.__dict__
-    res = True
-    for key in d1.keys():
-      res &= np.all( d1[key] == d2[key] )
-    return res
 
   def _pos_changed(self, old_pos, new_pos):
     """
@@ -269,25 +274,15 @@ class Geom_Object(object):
     sein für abgeleitete, zusammengesetzte Objekte wie beam oder composition
     """
     pass
-
-  # def _normal_changed(self, old_normal, new_normal):
-  #   """
-  #   deprecated, nutze _axes_changed
-    
-  #   wird aufgerufen, wen die Normale von <self> verändert wird, kann nützlich
-  #   sein für abgeleitete, zusammengesetzte Objekte wie beam oder composition
-
-  #   dreht außerdem das eigene Koordiantensystem
-  #   """
-  #   old_axes = self.get_axes()
-  #   new_axes = self._updated_axes(old_normal)
-  #   self._axes = new_axes
-  #   self._axes_changed(old_axes, new_axes)
     
     
   def _axes_changed(self, old_axes, new_axes):
+    """
+    wird aufgerufen, wen das Koordinatensystem >_axes> von <self> verändert 
+    wird,kann nützlich sein für abgeleitete, zusammengesetzte Objekte wie beam 
+    oder composition
+    """
     pass
-    # self.__normal = self._axes[:,0]
     
     
   def _rearange_subobjects_axes(self, old_axes, new_axes, objs):
@@ -306,24 +301,6 @@ class Geom_Object(object):
       obj.pos = new_qvec + p0
       obj.set_axes( np.matmul( RotM, obj.get_axes() ) )
       
-  # def _rearange_subobjects_normal(self, old_normal, new_normal, objs):
-  #   """
-  #   deprecated, wird jetzt über "_changed_axes" erledigt 
-    
-  #   wichtig für beam und Composition
-  #   wenn eigene <old_normal> auf <new_normal> geändert wird, soll die normal
-  #   aller Subobjekte entsprechend geändert werden, d.h. relative Ausrichtung
-  #   und Drehung soll beibehalten werden
-  #   ...LinAlg Kram halt
-  #   """
-  #   p0 = self.pos
-  #   RotM = rotation_matrix_from_vectors(old_normal, new_normal)
-  #   for obj in objs:
-  #     qvec = obj.pos - p0 # relativer Orstvector innnerhalb der Einheit
-  #     new_qvec = np.matmul(RotM, qvec)
-  #     obj.pos = new_qvec + p0
-  #     obj.normal = np.matmul(RotM, obj.normal)
-  #     # das sollte es gewesen sein
 
   def _rearange_subobjects_pos(self, old_pos, new_pos, objs):
     """
@@ -336,9 +313,19 @@ class Geom_Object(object):
     delta_pos = new_pos - old_pos
     for obj in objs:
       obj.pos += delta_pos
-      # das sollte es gewesen sein
 
-
+  # def is_equal(self, obj):
+  #   """
+  #   checkt ob <self> und obj äquivalent sind, also ob sie die gleichen Einträge
+  #   haben
+  #   returns True/False
+  #   """
+  #   d1 = self.__dict__
+  #   d2 = obj.__dict__
+  #   res = True
+  #   for key in d1.keys():
+  #     res &= np.all( d1[key] == d2[key] )
+  #   return res
 
 
 def tests():
@@ -356,25 +343,26 @@ def tests():
   print()
 
   g = Geom_Object()
+  g2 = Geom_Object()
   xvec = np.array((1,0,0))
-  print("Winkel von g zur x-Achse default", g.angle_to(xvec))
-  print("KooSys von g default x y z:", g.get_axes())
+  print("Winkel von g zur x-Achse default", g.angle_to(g2))
+  print("KooSys von g default x y z:", g.get_coordinate_system())
   yvec = np.array((0,1,0))
   g.normal = (yvec)
-  print("KooSys von g nach Drehung auf yAchse x y z:", g.get_axes())
+  print("KooSys von g nach Drehung auf yAchse x y z:", g.get_coordinate_system())
   g.normal = -xvec
-  print("KooSys von g nach Drehung auf -xAchse x y z:", g.get_axes())
+  print("KooSys von g nach Drehung auf -xAchse x y z:", g.get_coordinate_system())
   print("sollte bei (-1,0,0), (0,-1,0) und (0,0,1) liegen")
 
   print()
 
   h = Geom_Object()
   zvec = np.array((0,0,1))
-  print("KooSys von h default x y z:", h.get_axes())
+  print("KooSys von h default x y z:", h.get_coordinate_system())
   h.normal = (zvec)
-  print("KooSys von h nach Drehung auf zAchse x y z:", h.get_axes())
+  print("KooSys von h nach Drehung auf zAchse x y z:", h.get_coordinate_system())
   h.normal = -xvec
-  print("KooSys von h nach Drehung auf -xAchse x y z:", h.get_axes())
+  print("KooSys von h nach Drehung auf -xAchse x y z:", h.get_coordinate_system())
   print("sollte bei (-1,0,0), (0,1,0) und (0,0,-1) liegen")
 
   return x, g, h
--- main.py
+++ main.py
@@ -19,30 +19,12 @@ sys.path.append(pfad)
 
 from basic_optics.freecad_models import clear_doc, setview, freecad_da, freecad_model_lens
 from basic_optics import Beam, Mirror, Opt_Element, Geom_Object, Curved_Mirror, Lens,Iris,Diaphragms, Ray, Composition, inch, Grating, Propagation
-#from basic_optics.composition import Teleskop_test, Composition_mirror_test, Mirror_Teleskop_test, add_only_elem_test
 from basic_optics.mirror import curved_mirror_test
+from basic_optics.tests import all_moduls_test
 
 if freecad_da:
   clear_doc()
 
-
-
-# from basic_optics.freecad_models.freecad_model_mirror import model_stripe_mirror,model_round_mirror
-
-
-
-
-# from basic_optics.tests import Telescope_4beam, Lens_4beam_Fokus, Parallel_ray_bundle_tilted_lens, grating_ray_bundle_test
-
-# Telescope_4beam()
-Lens_4beam_Fokus()
-
-print("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!")
-
-# Parallel_ray_bundle_tilted_lens()
-# grating_ray_bundle_test()
-
-# from basic_optics.tests import all_moduls_test
 
 # peris, teles, amp, stretch, wcell = all_moduls_test()
 
@@ -50,49 +32,18 @@ print("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!")
 
 from basic_optics.moduls import Make_Amplifier_Typ_I_simpler,Make_Stretcher,Make_Amplifier_Typ_II_simpler,Make_Amplifier_Typ_II_simple,Make_Amplifier_Typ_I_simple
 from basic_optics.moduls import diaphragms_test
-# peris = Make_Telescope()
-# peris.draw()
-# teles = Make_Periscope()
-# teles.draw_elements()
-# teles.draw_mounts()
-# amplifier0 = Make_Amplifier_Typ_I_simple(beam_sep=15,roundtrips2=2)
-# amplifier0.pos = (0, 0, 100)
-# amplifier0.draw_elements()
-# # amplifier0.draw_rays()
-# amplifier0.draw_mount()
-# amplifier0.draw_beams()
 
-# amplifier1 = Make_Amplifier_Typ_I_simpler(beam_sep=20,roundtrips2=2)
-# amplifier1.pos = (0, 200, 100)
-# amplifier1.draw_elements()
-# # amplifier1.draw_rays()
-# amplifier1.draw_mounts()
-# amplifier1.draw_beams()
+# stretch = Make_Stretcher()
+# stretch.pos += (0,0,100)
+# stretch.draw_elements()
+# stretch.draw_rays()
+# stretch.draw_mounts()
 
-# amplifier2 = Make_Amplifier_Typ_II_simple(beam_sep=15,roundtrips2=2)
-# amplifier2.pos = (0, 400, 100)
-# amplifier2.draw_elements()
-# # amplifier2.draw_rays()
-# amplifier2.draw_mount()
-# amplifier2.draw_beams()
-
-# amplifier3 = Make_Amplifier_Typ_II_simpler(beam_sep=20,roundtrips2=2)
-# amplifier3.pos = (0, 600, 100)
-# amplifier3.draw_elements()
-# # amplifier3.draw_rays()
-
-# amplifier3.draw_mounts()
-# amplifier3.draw_beams()
-
-dia = diaphragms_test()
-dia.pos = (0,0,100)
-dia.draw_elements()
-dia.draw_rays()
-#dia.draw_beams()
-dia.draw_mounts()
+amp = Make_Amplifier_Typ_II_simpler()
+amp.pos = (0,0,200)
+amp.draw()
 
 if freecad_da:
-  #model_stripe_mirror(dia=152.4,Radius1=-1000)
-  #model_round_mirror(dia=152.4, thickness=30,Radius=500)
+
   setview()
 
