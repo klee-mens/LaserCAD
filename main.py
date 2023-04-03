7f23d00c94b99ae33dd9c35837c53852e32b997d
He Zhuang <zhuanghe199@gmail.com>
2023-04-03 10:06:05 +0200

    test the cavity

--- main.py
+++ main.py
@@ -22,74 +22,78 @@ import numpy as np
 
 # from basic_optics.mirror import curved_mirror_test
 from basic_optics.tests import all_moduls_test
+from basic_optics.moduls import Make_White_Cell
 
 if freecad_da:
   clear_doc()
 
-peris, teles, amp, stretch, wcell = all_moduls_test()
+# peris, teles, amp, stretch, wcell = all_moduls_test()
 
 from basic_optics.moduls import Make_Telescope,Make_Amplifier_Typ_I_simpler,Make_Stretcher,Make_Amplifier_Typ_II_simpler,Make_Amplifier_Typ_II_simple,Make_Amplifier_Typ_I_simple
 from basic_optics.moduls import diaphragms_test
 
 from basic_optics.tests import iris_test
 
-# # rg=Beam(radius=2.5,angle=0)
+# rg=Beam(radius=2.5,angle=0)
 # rg.make_square_distribution(15)
 # rg.draw()
 
-# rg=Beam(radius=2.5,pos=(0,0,100))
 
-# cm_radius = 200
-# cavity_length = 425
-# angle_shift = 2.796834341
-# l_from_m1_to_cm1 = 1/(2/cm_radius - 2/cavity_length) - 35
-# cm1_x = l_from_m1_to_cm1*np.cos(angle_shift*2/180*np.pi)
+cm_radius = 200
+cavity_length = 425
+angle_shift = 2.796834341
+cav_height = 100
+l_from_m1_to_cm1 = 1/(2/cm_radius - 2/cavity_length) - 35
+cm1_x = l_from_m1_to_cm1*np.cos(angle_shift*2/180*np.pi)
 
-# rg=Beam(radius=0.1,angle=0.05)
-# cavset=Composition(name="Cavity Setting")
-# cavset.set_light_source(rg)
-# cavset.normal=(0,-1,0)
-# cavset.pos=(0,20,100)
+ls = Beam(radius=0.1,angle=0.05, pos=(0,0,cav_height))
+cavset=Composition(name="Cavity Setting")
+cavset.set_light_source(ls)
+cavset.normal=(0,-1,0)
+cavset.pos=(0,20,cav_height)
 
-# m1 = Mirror()
-# m1.pos = (0,-15,100)
-# point0 = (0,20,100)
-# point1 = (-153.1560972,0,100) 
-# m1.set_normal_with_2_points(point0, point1)
-# m1.aperture=25.4/2
+m1 = Mirror()
+m1.pos = (0,-15,cav_height)
+point0 = (0,20,cav_height)
+point1 = (-cm1_x,0,cav_height) 
+m1.set_normal_with_2_points(point0, point1)
+m1.aperture=25.4/2
 
-# cm1 = Curved_Mirror(radius= 200)
-# cm1.pos = (-153.1560972,0,100) 
-# cm1.normal = (-1,0,0)
-# point1 = (0,-15,100)
-# point0 = cm1.pos+(425,0,0)
-# cm1.set_normal_with_2_points(point0, point1)
-# cm1.aperture = 25.4*2
+cm1 = Curved_Mirror(radius= cm_radius)
+cm1.pos = (-cm1_x,0,cav_height) 
+cm1.normal = (-1,0,0)
+point1 = (0,-15,cav_height)
+point0 = cm1.pos+(cavity_length,0,0)
+cm1.set_normal_with_2_points(point0, point1)
+cm1.aperture = 25.4*2
 
-# cm2 = Curved_Mirror(radius= 200,theta=-2.796834341*2)
-# cm2.pos = cm1.pos+(425,0,0)
-# cm2.aperture = 25.4*2
+cm2 = Curved_Mirror(radius= cm_radius,theta=-angle_shift*2)
+cm2.pos = cm1.pos+(cavity_length,0,0)
+cm2.aperture = 25.4*2
 
-# m2 = Mirror()
-# m2.pos = cm2.pos -(173.06080608,0,4695/277)
-# point0 = cm2.pos
-# point1 = m2.pos - (0,15,0)
-# m2.set_normal_with_2_points(point0, point1)
-# m2.aperture=25.4/2
+l_from_m2_to_cm2 = 1/(2/cm_radius-2/cavity_length) - 15
+cm2_x =l_from_m2_to_cm2*np.cos(angle_shift*2/180*np.pi)
+cm2_z = l_from_m2_to_cm2*np.sin(angle_shift*2/180*np.pi)
+m2 = Mirror()
+m2.pos = cm2.pos -( cm2_x,0, cm2_z)
+point0 = cm2.pos
+point1 = m2.pos - (0,15,0)
+m2.set_normal_with_2_points(point0, point1)
+m2.aperture=25.4/2
 
 
-# ip = Intersection_plane()
-# ip.pos = m2.pos - (0,13.17,0)
-# ip.normal = (0,-1,0)
+ip = Intersection_plane()
+ip.pos = m2.pos - (0,13.17,0)
+ip.normal = (0,-1,0)
 
-# cavset.add_fixed_elm(m1)
-# cavset.add_fixed_elm(cm1)
-# cavset.add_fixed_elm(cm2)
-# cavset.add_fixed_elm(m2)
-# cavset.add_fixed_elm(ip)
-# cavset.propagate(25)
+cavset.add_fixed_elm(m1)
+cavset.add_fixed_elm(cm1)
+cavset.add_fixed_elm(cm2)
+cavset.add_fixed_elm(m2)
+cavset.add_fixed_elm(ip)
+cavset.propagate(25)
 
-# cavset.draw()
+cavset.draw()
 
 # table= model_table()
 
@@ -139,7 +143,7 @@ from basic_optics.tests import iris_test
 
 # cavset.draw()
 
-iris_test()
+# iris_test()
 
 
 from basic_optics.tests import Intersection_plane_spot_diagram_test
