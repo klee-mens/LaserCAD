fix the bug of extend fix the bug of rotation

He_main.py
@@ -22,6 +22,7 @@ from basic_optics.freecad_models import clear_doc, setview, freecad_da, freecad_
 from basic_optics import Opt_Element, Geom_Object, Curved_Mirror,Thick_Lens
 from basic_optics import Ray, Composition, Grating, Propagation
 from basic_optics import Refractive_plane
+from basic_optics.freecad_models import add_to_composition
 
 # from basic_optics.mirror import curved_mirror_test
 from basic_optics.tests import all_moduls_test
@@ -235,13 +236,13 @@ for wavel in wavels:
 lightsource.override_rays(rays)
 
 newlightsource0 = Beam(radius=1, angle=0,wavelength=lam_mid)
-newlightsource0.make_circular_distribution(ring_number=3)
+newlightsource0.make_circular_distribution(ring_number=2)
 
 newlightsource1 = Beam(radius=1, angle=0,wavelength=lam_mid+delta_lamda/2)
-newlightsource1.make_circular_distribution(ring_number=3)
+newlightsource1.make_circular_distribution(ring_number=2)
   
 newlightsource2 = Beam(radius=1, angle=0,wavelength=lam_mid-delta_lamda/2)
-newlightsource2.make_circular_distribution(ring_number=3)
+newlightsource2.make_circular_distribution(ring_number=2)
   
 newlightsource = Beam(radius=0, angle=0)
 r=Ray()
@@ -304,7 +305,7 @@ point0 = p_grat
 point1 = M3.pos + (-100,0,0)
 M3.set_normal_with_2_points(point0, point1)
 
-Curved_Mirror2 = Curved_Mirror(radius=3000, name="Concav_Mirror")
+Curved_Mirror2 = Curved_Mirror(radius=4000, name="Concav_Mirror")
 Curved_Mirror2.aperture = 25.4*2
 Curved_Mirror2.pos = M3.pos + (-100,0,0)
 Curved_Mirror2.normal = (-1,0,0)
@@ -319,7 +320,7 @@ fixlens2.aperture = 25.4/2
 fixlens2.pos = p_grat - 425 * vec
 fixlens2.normal = vec
 
-Curved_Mirror1 = Curved_Mirror(radius=3000, name="Concav_Mirror")
+Curved_Mirror1 = Curved_Mirror(radius=4000, name="Concav_Mirror")
 Curved_Mirror1.aperture = 25.4*2
 Curved_Mirror1.pos = M1.pos + (250,0,0)
 Curved_Mirror1.normal = (1,0,0)
@@ -377,36 +378,50 @@ Stretcher.add_fixed_elm(pure_cosmetic)
 # for item in subperis._elements:
 #   Stretcher.add_fixed_elm(item)
 
-
-seq = [0,1,2,3,0,4,5,0,6,2,7,0,8,9,8,0,7,2,6,0,5,4,0,3,2,1,0,10,11,10]
+# seq = [0,1,2,3,0,4,5,0,6,2,7,0,8,9,8,0,7,2,6,0,5,4,0,3,2,1,0,10,11,10]
 # seq = [0,1,2,1,0, 3]
 
 # seq = [0,1,2,3,0, 4, 5, 6, 2, 7, 0]
 
-roundtrip_sequence = seq
+# from collections import deque
+
+seq = np.array([0,1,2,3,0,4,5,0,6,2,7,0,8,9,8,0,7,2,6,0,5,4,0,3,2,1,0,10,11,10])
+roundtrip_sequence = list(seq)
 
 # Concav1.draw()
 # Grat.draw()
 
-roundtrip=5
+roundtrip=10
+# seq = np.repeat(roundtrip_sequence, roundtrip)
 
 for n in range(roundtrip-1):
-  seq.extend(roundtrip_sequence)
+  # print("step ", n, "of", roundtrip)
+  seq = np.append(seq,roundtrip_sequence)
   
-# seq.extend([0,1])
-
-seq.extend([12])
+seq=np.append(seq, [12])
 
 Stretcher.set_sequence(seq)
-# Stretcher.propagate(100)
-# Stretcher.pos = (0,0,100)
+Stretcher.propagate(100)
+Stretcher.pos = (0,0,100)
 
-# Stretcher.draw_mounts()
+Stretcher.draw_mounts()
 Stretcher.draw_elements()
-# Stretcher.compute_beams()
+Stretcher.compute_beams()
 
-Stretcher.draw_beams(style="ray_group")
+container = []
+for n in range(-32,1):
+  beam = Stretcher._beams[n]
+  beam.draw_dict["model"] = "ray_group"
+  obj = beam.draw()
+  container.append(obj)
+if freecad_da:
+  part = add_to_composition(Stretcher._beams_part, container)
+else:
+  for x in container:
+    Stretcher._beams_part.append(x)
+# Stretcher.draw_beams(style="ray_group")
 ip.spot_diagram(Stretcher._beams[-1])
+
 
 # for n in range(roundtrip-1):
 #   rays = Stretcher._beams[-1].get_all_rays()
@@ -452,6 +467,7 @@ ip.spot_diagram(Stretcher._beams[-1])
 #   else:
 #     seq.extend([12])
 #     Stretcher.set_sequence(seq)
+#     Stretcher.draw_elements()
 #     Stretcher.draw_beams(style="ray_group")
 #     ip.spot_diagram(Stretcher._beams[-1])
 

basic_optics/freecad_models/utils.py
@@ -222,11 +222,12 @@ def update_geom_info(obj, geom_info, off0=0):
       pos_norm=np.array((geom_info[0],normal))
       update_pos_norm(obj,pos_norm,off0=off0)
     else:
-      # print(axes)
+      # print("axes=",axes)
       rotvec, phi = rotation_to_axis_angle(axes)
       rotvec = Vector(rotvec)
       phi *= 180/np.pi
       # print(rotvec, phi)
       # print(rotvec,phi)
       place0 = obj.Placement
+      # print("phi=",phi)
       obj.Placement = Placement(pos, Rotation(rotvec,phi), Vector(0,0,0)).multiply(place0)

basic_optics/mirror.py
@@ -337,12 +337,12 @@ class Cylindrical_Mirror(Mirror):
     # self.draw_dict["mount_type"] = "POLARIS-K1-Step"
     self.draw_dict["Radius1"] = self.radius
     obj = model_mirror(**self.draw_dict)
-    default = Vector(0,0,1)
-    xx,yy,zz = self.get_coordinate_system()
-    zz = Vector(zz)
-    angle = default.getAngle(zz)*180/np.pi
-    vec = default.cross(zz)
-    rotate(obj, vec, angle, off0=0)
+    # default = Vector(0,0,1)
+    # xx,yy,zz = self.get_coordinate_system()
+    # zz = Vector(zz)
+    # angle = default.getAngle(zz)*180/np.pi
+    # vec = default.cross(zz)
+    # rotate(obj, vec, angle, off0=0)
     return obj
   
   def next_ray_tracing(self, ray):

he_mount_tests.py
@@ -58,18 +58,18 @@ if freecad_da:
 # gb2.draw()
 # gb3.draw()
 # ==========================================================
-Radius = 600 #Radius des großen Konkavspiegels
-Aperture_concav = 100
-h_StripeM = 10 #Höhe des Streifenspiegels
-# gamma = 33.4906043205826 /180 *np.pi # Seperationswinkel zwischen einfallenden und Mittelpunktsstrahl; Alpha = Gamma + Beta
-gamma = 36 /180 *np.pi
-grat_const = 1/1480 # Gitterkonstante in 1/mm
-seperation = 120 # Differenz zwischen Gratingposition und Radius
-lam_mid = 1030e-9 * 1e3 # Zentralwellenlänge in mm
-delta_lamda = 50e-9*1e3 # Bandbreite in mm
-number_of_rays = 20
-safety_to_StripeM = 5 #Abstand der eingehenden Strahlen zum Concav Spiegel in mm
-periscope_distance = 10
+# Radius = 600 #Radius des großen Konkavspiegels
+# Aperture_concav = 100
+# h_StripeM = 10 #Höhe des Streifenspiegels
+# # gamma = 33.4906043205826 /180 *np.pi # Seperationswinkel zwischen einfallenden und Mittelpunktsstrahl; Alpha = Gamma + Beta
+# gamma = 36 /180 *np.pi
+# grat_const = 1/1480 # Gitterkonstante in 1/mm
+# seperation = 120 # Differenz zwischen Gratingposition und Radius
+# lam_mid = 1030e-9 * 1e3 # Zentralwellenlänge in mm
+# delta_lamda = 50e-9*1e3 # Bandbreite in mm
+# number_of_rays = 20
+# safety_to_StripeM = 5 #Abstand der eingehenden Strahlen zum Concav Spiegel in mm
+# periscope_distance = 10
 # ray0 = Ray(pos=[208.49408, 137.95006, 100.     ],normal=[-0.83397755, -0.55179838,  0.        ])
 # ray0 = Ray(pos=[0,0,0],normal=[1,1,0])
 # gb1 = Beam(radius=10, angle=0.1,wavelength=1030E-3,pos=(0,0,0))
@@ -78,17 +78,29 @@ periscope_distance = 10
 # ray1 = Ray(pos=(0,0,100))
 # ray1.length=600
 # Radius = 600
-StripeM = Cylindrical_Mirror(radius = Radius,pos=(0,0,100))
-p0=p1=(-100,0,100)
-StripeM.set_normal_with_2_points(p0, p1)
-StripeM.normal=-StripeM.normal
-StripeM.aperture=75
-StripeM.draw_dict["height"]=9
-StripeM.draw_dict["thickness"]=25
-StripeM.draw_dict["model_type"]="Stripe"
-StripeM.draw()
-StripeM.draw_mount()
 
+# StripeM = Cylindrical_Mirror(radius = 600,pos=(0,0,100))
+# p0=p1=(-100,0,100)
+# StripeM.normal = (1,1,0)
+# # StripeM.set_normal_with_2_points(p0, p1)
+# # StripeM.normal=-StripeM.normal
+# # StripeM.normal = (1,0,0)
+# # a = StripeM.get_axes()
+# # print(a)
+# StripeM.rotate((1,1,0),np.pi/2)
+# # a = StripeM.get_axes()
+# # print(a)
+# StripeM.aperture=75
+# StripeM.draw_dict["height"]=9
+# StripeM.draw_dict["thickness"]=25
+# StripeM.draw_dict["model_type"]="Stripe"
+
+# StripeM.draw()
+# StripeM.draw_mount()
+from basic_optics.moduls import Make_Periscope, Make_Telescope, Make_Amplifier_Typ_II_simple, Make_Stretcher, Make_White_Cell
+a=Make_White_Cell(roundtrips4=8*10)
+ls = Beam()
+a.compute_beams()
 # Concav1 = Cylindrical_Mirror(radius=Radius, name="Concav_Mirror")
 # Concav1.pos = [-277.74575141,  146.94631307,  100.        ]
 # Concav1.aperture = Aperture_concav