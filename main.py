50211b41f9a67539eec60603d8c857dce48d069e
Clemens <clemens.anschuetz@uni-jena.de>
2023-05-04 17:52:08 +0200

    Presentation version with examples

--- main.py
+++ main.py
@@ -8,46 +8,54 @@ i@author: mens
 
 import sys
 import os
+import numpy as np
 
 pfad = __file__
 pfad = pfad[0:-7] #nur wenn das Skript auch wirklich main.py heißt
 sys.path.append(pfad)
+inch = 25.4
 
 
 from basic_optics.freecad_models import clear_doc, setview, freecad_da, freecad_model_lens
-
 from basic_optics import Beam, Mirror, Opt_Element, Geom_Object, Curved_Mirror
 from basic_optics import Lens, Ray, Composition, Grating, Propagation
-
-# from basic_optics.mirror import curved_mirror_test
 from basic_optics.tests import all_moduls_test
+from basic_optics.moduls import Make_Stretcher
+
 
 if freecad_da:
   clear_doc()
 
-inch = 25.4
-import numpy as np
+
+stretcher = Make_Stretcher()
+stretcher.pos=(0,0,120)
+stretcher.draw_elements()
+stretcher.draw_mounts()
+stretcher.draw_rays()
 
 
+# teles = Composition(name="KepplerTelescope")
+# liso = Beam(radius=1, angle=0)
+# teles.set_light_source(liso)
+# f1 = 100
+# f2 = 300
+# le1 = Lens(f=f1)
+# le2 = Lens(f=f2)
 
-from basic_optics.moduls import Make_Amplifier_Typ_II_Juergen
-from basic_optics.tests import all_moduls_test
+# teles.propagate(f1)
+# teles.add_on_axis(le1)
+# teles.propagate(f1+f2)
+# teles.add_on_axis(le2)
+# teles.propagate(f2)
 
+# flip1 = Mirror(phi=65)
+# flip1.draw_dict["mount_type"] = "KS1"
 
-# amp = Make_Amplifier_Typ_II_Juergen()
-# amp.draw()
+# teles.add_on_axis(flip1)
+# teles.propagate(80)
 
-# all_moduls_test()
-
-# amp = Make_Amplifier_Typ_II_Juergen()
-# amp.draw()
-
-from basic_optics.resonator import Resonator
-from basic_optics.tests import simple_resonator_test
-
-res = simple_resonator_test()
+# teles.draw()
 
 
 if freecad_da:
-
   setview()
