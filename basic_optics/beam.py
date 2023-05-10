Fix matrix calculation in Resonator
Gaussian_Beam is better than just distribution
three_resonators_test gives 3 examples of resonators correctly

basic_optics/beam.py
@@ -33,13 +33,13 @@ class Beam(Geom_Object):
     self._Bwavelength = wavelength
     if distribution == "cone":
       self.make_cone_distribution()
-      self.draw_dict["model"] = "cone"      
+      self.draw_dict["model"] = "cone"
     elif distribution == "square":
       self.make_square_distribution()
-      self.draw_dict["model"] = "ray_group"      
+      self.draw_dict["model"] = "ray_group"
     elif distribution == "circular":
       self.make_circular_distribution()
-      self.draw_dict["model"] = "ray_group" 
+      self.draw_dict["model"] = "ray_group"
     elif distribution == "Gaussian":
       z0 = wavelength/(np.pi*np.tan(angle)*np.tan(angle))
       w0 = wavelength/(np.pi*np.tan(angle))
@@ -59,7 +59,7 @@ class Beam(Geom_Object):
       print("Allowed distribution types are: 'cone', 'square', 'circular', 'Gaussian'")
       self = -1
       return None
-    
+
   def make_cone_distribution(self, ray_count=2):
     self._ray_count = ray_count
     self._distribution = "cone"
@@ -72,7 +72,7 @@ class Beam(Geom_Object):
       our = self._rays[n+1]
       our.from_h_alpha_theta(self._radius, self._angle, thetas[n], self)
       our.name = self.name + "_outer_Ray" + str(n)
-  
+
   def make_Gaussian_distribution(self, ray_count=2):
     self._ray_count = 1
     self._distribution = "Gaussian"
@@ -81,7 +81,7 @@ class Beam(Geom_Object):
     mr.set_geom(self.get_geom())
     mr.name = self.name + "_inner_Ray"
 
-  
+
   def make_square_distribution(self, ray_in_line=3):
     """
     Let the group of rays follow the square distribution
@@ -133,7 +133,7 @@ class Beam(Geom_Object):
     ray_counting=0
     radius=self._radius
     for r in np.arange(0,radius+radius/ring_number/2,radius/ring_number):        #r repersents the height of the ray
-      
+
       if r!=0:                                                                 #if the ray is not in the center
         thetas = np.linspace(0, 2*np.pi, int(r*ring_number/radius)*6+1)        #thetas repersents the rotation angle of the ray
         for n in range(int(r*ring_number/radius)*6):
@@ -225,7 +225,7 @@ class Beam(Geom_Object):
 
   def length(self):
     return self.inner_ray().length
-  
+
   def set_length(self, x):
     for ray in self._rays:
       ray.length = x
@@ -279,7 +279,7 @@ class Beam(Geom_Object):
         obj = our.draw_fc()
         container.append(obj)
       add_to_composition(part, container)
-      return part    
+      return part
 
 
 
@@ -299,11 +299,12 @@ class Gaussian_Beam(Ray):
     q_para = complex(z,z0)
     self.wavelength = wavelength
     self.q_para = q_para
+    self._distribution = "Gaussian"
 
   def set_length(self, length):
     # needed for consitency in next_beam function
     self.length = length
-    
+
   def __repr__(self):
     # radius, angle = self.radius_angle()
     n = len(self.class_name())

basic_optics/composition.py
@@ -28,7 +28,7 @@ class Composition(Opt_Element):
     super().__init__(name=name,**kwargs)
     oA = Ray(name=self.name+"__oA_0", pos=self.pos, normal=self.normal)
     oA.length = 0
-    self.__optical_axis = [oA]
+    self._optical_axis = [oA]
     self._elements = []
     self._sequence = []
     self._last_prop = 0 #für den Fall, dass eine letzte Propagation nach einem Element noch erwünscht ist
@@ -58,14 +58,14 @@ class Composition(Opt_Element):
     x : float
       Länge um die propagiert wird
     """
-    end_of_axis = self.__optical_axis[-1]
+    end_of_axis = self._optical_axis[-1]
     end_of_axis.length += x
     # self._last_geom = (end_of_axis.endpoint(), end_of_axis.normal)
     # self._matrix = np.matmul( np.array([[1,x], [0,1]]), self._matrix) #braucht keine Sau
     self._last_prop = x #endet mit Propagation
 
   def last_geom(self):
-    end_of_axis = self.__optical_axis[-1]
+    end_of_axis = self._optical_axis[-1]
     return (end_of_axis.endpoint(), end_of_axis.get_axes())
 
   def __add_raw(self, item):
@@ -92,11 +92,11 @@ class Composition(Opt_Element):
     # item.name = self.new_catalogue_entry(item)
     item.set_geom(self.last_geom())
     self.__add_raw(item)
-    newoA_try = item.next_ray(self.__optical_axis[-1])
+    newoA_try = item.next_ray(self._optical_axis[-1])
     if newoA_try:
-      newoA = item.next_ray(self.__optical_axis[-1])
+      newoA = item.next_ray(self._optical_axis[-1])
       newoA.length = 0
-      self.__optical_axis.append(newoA)
+      self._optical_axis.append(newoA)
 
   def add_fixed_elm(self, item):
     """
@@ -122,33 +122,48 @@ class Composition(Opt_Element):
     self.set_geom(ray.get_geom())
     oA = deepcopy(ray)
     oA.name = self.name +"__oA_0"
-    self.__optical_axis = [oA]
+    self._optical_axis = [oA]
     self.recompute_optical_axis()
 
   def recompute_optical_axis(self):
-    self.__optical_axis = [self.__optical_axis[0]]
-    counter = 1
-    
+    self._optical_axis = [self._optical_axis[0]]
+    counter = 0
+
     for ind in self._sequence:
       elm = self._elements[ind]
       # print("-----", elm)
-      newoA = elm.next_ray(self.__optical_axis[-1])
+      newoA = elm.next_ray(self._optical_axis[-1])
       counter += 1
       newoA.name = self.name + "__oA_" + str(counter)
-      self.__optical_axis.append(newoA)
-    self.__optical_axis[-1].length = self._last_prop
+      self._optical_axis.append(newoA)
+    self._optical_axis[-1].length = self._last_prop
 
 
   def matrix(self):
-   self._matrix = np.eye(2)
-   self.recompute_optical_axis()
-   for ind in self._sequence:
-     B = self.__optical_axis[ind].length
-     M = self._elements[ind]._matrix
-     self._matrix = np.matmul(np.array([[1,B], [0,1]]), self._matrix )
-     self._matrix = np.matmul(M, self._matrix )
-   self._matrix = np.matmul(np.array([[1,self._last_prop], [0,1]]), self._matrix )
-   return np.array(self._matrix)
+    """
+    computes the optical matrix of the system
+    each iteration consists of a propagation given by the length of the nth
+    ray of the optical_axis followed by the matrix multiplication with the
+    seq[n] element
+
+    Returns the ABCD-matrix
+    """
+    self._matrix = np.eye(2)
+    self.recompute_optical_axis()
+    counter = -1
+    for ind in self._sequence:
+      counter += 1
+      B = self._optical_axis[counter].length
+      M = self._elements[ind]._matrix
+      self._matrix = np.matmul(np.array([[1,B], [0,1]]), self._matrix )
+      # print("--")
+      # print(B)
+      # print(M)
+      # print("--")
+      self._matrix = np.matmul(M, self._matrix )
+    # self._matrix = np.matmul(np.array([[1,self._last_prop], [0,1]]), self._matrix )
+    self._matrix = np.matmul(np.array([[1,self._last_prop], [0,1]]), self._matrix ) #last propagation
+    return np.array(self._matrix)
 
   def get_sequence(self):
     return list(self._sequence)
@@ -173,7 +188,8 @@ class Composition(Opt_Element):
         beamcount += 1
         beam.name = self.name + "_beam_" + str(beamcount)
         beamlist.append(beam)
-    beam.set_length(self._last_prop)
+    # beam.set_length(self._last_prop)
+    beamlist[-1].set_length(self._last_prop)
     if not external_source:
       self._beams = beamlist
     return beamlist
@@ -210,10 +226,10 @@ class Composition(Opt_Element):
     #   print("-------------------------------------------------------")
     #   container.append(obj)
     return self.__container_to_part(self._mounts_part, container)
-  
+
   def draw_rays(self):
     return self.draw_beams(style="ray_group")
-  
+
 
   def draw(self):
     self.draw_elements()
@@ -300,7 +316,7 @@ class Composition(Opt_Element):
     self._rearange_subobjects_pos(old_pos, new_pos, [self._lightsource]) #sonst wird ls doppelt geshifted
     self._rearange_subobjects_pos(old_pos, new_pos, self._beams[1::])
     self._rearange_subobjects_pos(old_pos, new_pos, [r for rg in self._ray_groups[1::] for r in rg])
-    self._rearange_subobjects_pos(old_pos, new_pos, self.__optical_axis)
+    self._rearange_subobjects_pos(old_pos, new_pos, self._optical_axis)
 
   def _axes_changed(self, old_axes, new_axes):
     """
@@ -314,7 +330,7 @@ class Composition(Opt_Element):
     self._rearange_subobjects_axes(old_axes, new_axes, self._elements)
     self._rearange_subobjects_axes(old_axes, new_axes, self._beams[1::])
     self._rearange_subobjects_axes(old_axes, new_axes, [r for rg in self._ray_groups[1::] for r in rg])
-    self._rearange_subobjects_axes(old_axes, new_axes, self.__optical_axis)
+    self._rearange_subobjects_axes(old_axes, new_axes, self._optical_axis)
 
 
 

basic_optics/optical_element.py
@@ -80,7 +80,7 @@ class Opt_Element(Geom_Object):
     newb.name = "next_" + beam.name
     rays = beam.get_all_rays(by_reference=True)
     # if beam._distribution == "Gaussian":
-      
+
     newrays = []
     for ray in rays:
       nr = self.next_ray(ray)
@@ -175,9 +175,9 @@ class Opt_Element(Geom_Object):
     norm2 = np.cos(alpha2)*ea + np.sin(alpha2)*em
     ray2.pos = pos2
     ray2.normal = norm2
-    
+
     return ray2
-    
+
 
   def draw_mount(self):
     if freecad_da:

basic_optics/resonator.py
@@ -8,7 +8,7 @@ Created on Thu Apr 27 00:51:06 2023
 
 from .composition import Composition
 from .mirror import Mirror
-from .beam import Gaussian_Beam
+from .beam import Gaussian_Beam, Beam
 import numpy as np
 
 class Resonator(Composition):
@@ -23,7 +23,7 @@ class Resonator(Composition):
     super().__init__(**kwargs)
     self._outputcoupler_index = 0
     self.wavelength = 1030e-6 #Yb in mm
-    
+
   def add_outputcoupler(self, item):
     if type(item) == type(Mirror()):
       self.add_on_axis(item)
@@ -31,14 +31,14 @@ class Resonator(Composition):
     else:
       print("Outputcoupler must be a mirror")
       return -1
-    
+
   def set_outputcoupler_index(self, index):
     if type(self._elements[index]) == type(Mirror()):
       self._outputcoupler_index = index
     else:
       print("Outputcoupler must be a mirror")
       return -1
-  
+
     def set_wavelength(self, wavelength):
       """
       sets the own wavelength and thus the one of the lightsource and eigenmode
@@ -46,7 +46,7 @@ class Resonator(Composition):
       """
       self.wavelength = wavelength
       self._lightsource.wavelength = wavelength
-  
+
   def compute_eigenmode(self, start_index=0):
     """
     computes the gaussian TEM00 eigenmode from the matrix law
@@ -74,18 +74,22 @@ class Resonator(Composition):
     if E < 0:
       print("Resonator is unstable")
       return -1
-    ### set Lightsource accordingly 
+    ### set Lightsource accordingly
     z0 = np.sqrt(E)
     q_para = (z +1j*z0)
     gb00 = Gaussian_Beam(wavelength=self.wavelength) #der -1 strahl
+    # gb00 = Beam(wavelength=self.wavelength, distribution="Gaussian") #der -1 strahl
     gb00.q_para = q_para
     gb00.set_geom(self.get_geom())
     lsgb = self._elements[0].next_beam(gb00)
     self._lightsource = lsgb
     # set sequence for compute beams
-    self.set_sequence([x for x in range(1, noe)])
+    # self.set_sequence([x for x in range(1, noe)])
+    prop = np.linalg.norm(self._elements[-1].pos-self._elements[-2].pos)
+    self._last_prop = prop
     return q_para
-  
+
   def compute_beams(self, external_source=None):
     self.compute_eigenmode()
+    self.set_sequence([x for x in range(1, len(self._elements)-1)])
     super().compute_beams(external_source)

basic_optics/tests.py
@@ -17,7 +17,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from .iris import Iris
-from .intersection_plane import Intersection_plane 
+from .intersection_plane import Intersection_plane
 
 from .resonator import Resonator
 
@@ -464,7 +464,7 @@ def cavity_test(cm_radius = 200, cavity_length = 425, angle_shift = 2.796834341,
                 cav_height = 100, ls_shift = 35, mr_shift =15,
                 aperture_big = 25.4*2, aperture_small = 25.4/2):
   """
-    
+
 
     Parameters
     ----------
@@ -495,7 +495,7 @@ def cavity_test(cm_radius = 200, cavity_length = 425, angle_shift = 2.796834341,
   m1_y = l_from_m1_to_cm1*np.sin(angle_shift*2/180*np.pi)
 
 
-  ls = Beam(radius=0.1,angle=0.05,wavelength=1030E-6, distribution="Gaussian", 
+  ls = Beam(radius=0.1,angle=0.05,wavelength=1030E-6, distribution="Gaussian",
             pos=(0,0,cav_height))
   cavset=Composition(name="Cavity Setting")
   cavset.set_light_source(ls)
@@ -505,12 +505,12 @@ def cavity_test(cm_radius = 200, cavity_length = 425, angle_shift = 2.796834341,
   m1 = Mirror()
   m1.pos = (0,-m1_y,cav_height)
   point0 = (0,ls_shift,cav_height)
-  point1 = (-cm1_x,0,cav_height) 
+  point1 = (-cm1_x,0,cav_height)
   m1.set_normal_with_2_points(point0, point1)
   m1.aperture = aperture_small
 
   cm1 = Curved_Mirror(radius= cm_radius)
-  cm1.pos = (-cm1_x,0,cav_height) 
+  cm1.pos = (-cm1_x,0,cav_height)
   cm1.normal = (-1,0,0)
   point1 = (0,-m1_y,cav_height)
   point0 = cm1.pos+(cavity_length,0,0)
@@ -544,10 +544,10 @@ def cavity_test(cm_radius = 200, cavity_length = 425, angle_shift = 2.796834341,
   cavset.propagate(25)
   # ip.spot_diagram(cavset._ray_groups[-1])
   cavset.draw()
-  
+
 def Reflective_plane_test(Beam_radius = 2.5, Beam_angle = 0.1):
   """
-    
+
 
     Parameters
     ----------
@@ -580,7 +580,7 @@ def Reflective_plane_test(Beam_radius = 2.5, Beam_angle = 0.1):
   re_test.propagate(50)
   re_test.draw_elements()
   re_test.draw_beams()
-  
+
 def Cylindrical_Mirror_test():
   """
   a simple Cylindrical_Mirror_test
@@ -605,7 +605,7 @@ def Cylindrical_Mirror_test():
   # rg1.length = 2000
   rg.draw()
   rg1.draw()
-  
+
 def Stretcher_Cavity_test():
     Radius = 1000 #Radius des großen Konkavspiegels
     Aperture_concav = 6 * 25.4
@@ -755,7 +755,7 @@ def Stretcher_Cavity_test():
     Stretcher.draw_elements()
     Stretcher.draw_mounts()
     Stretcher.draw_rays()
-    
+
 def Wrong_stretcher_with_two_Cylindrical_Mirror():
   Radius = 1000 #Radius des großen Konkavspiegels
   Aperture_concav = 6 * 25.4
@@ -916,7 +916,7 @@ def Wrong_stretcher_with_two_Cylindrical_Mirror():
   Stretcher.draw_elements()
   Stretcher.draw_mounts()
   Stretcher.draw_rays()
-  
+
 def simple_resonator_test():
   res = Resonator()
   m0 = Mirror()
@@ -930,3 +930,92 @@ def simple_resonator_test():
   res.add_on_axis(m1)
   res.draw()
   return res
+
+
+def three_resonators_test():
+  from basic_optics.resonator import Resonator
+
+  res = Resonator()
+  g = 0.2
+  L = 250
+  R = L / (1-g)
+  wavelength = 0.1
+  res.wavelength = wavelength
+  cm1 = Curved_Mirror(radius=R)
+  cm2 = Curved_Mirror(radius=R)
+  res.add_on_axis(cm1)
+  res.propagate(L)
+  res.add_on_axis(cm2)
+  res.draw()
+
+
+  alpha = -8
+  beta = -0.1
+  print("g1*g2 = ", alpha*beta)
+  focal = 250
+  dist1 = (1-alpha)*focal
+  dist2 = (1-beta)*focal
+  wavelength = 0.1
+  res2 = Resonator()
+  res2.pos += (0,100, 0)
+  mir1 = Mirror()
+  mir2 = Mirror()
+  le1 = Lens(f=focal)
+
+  res2.add_on_axis(mir1)
+  res2.propagate(dist1)
+  res2.add_on_axis(le1)
+  res2.propagate(dist2)
+  res2.add_on_axis(mir2)
+
+  q = res2.compute_eigenmode()
+
+  print()
+  print(res2.matrix())
+  print()
+
+  redu = (1-alpha*beta)*focal
+  mat2 = np.array([[beta*alpha - redu/focal, 2*alpha*redu], [-2*beta/focal, beta*alpha - redu/focal]])
+  print()
+  print()
+  print(mat2)
+  print()
+
+  res2.draw()
+
+
+
+  res3 = Resonator(name="foldedRes")
+  res3.pos += (0,-200, 0)
+
+  alpha = -8
+  beta = -0.1
+  print("g1*g2 = ", alpha*beta)
+  focal = 250
+  dist1 = (1-alpha)*focal
+  dist2 = (1-beta)*focal
+  wavelength = 0.1
+  frac1 = 0.4
+  frac2 = 0.1
+  frac3 = 1 - frac1 - frac2
+
+  mir1 = Mirror(phi=180)
+  mir2 = Mirror(phi=75)
+  mir3 = Mirror(phi=-75)
+  mir4 = Mirror(phi=180)
+  cm = Curved_Mirror(radius=focal*2, phi = 170)
+
+  res3.add_on_axis(mir1)
+  res3.propagate(dist1*frac1)
+  res3.add_on_axis(mir2)
+  res3.propagate(dist1*frac2)
+  res3.add_on_axis(mir3)
+  res3.propagate(dist1*frac3)
+  res3.add_on_axis(cm)
+  res3.propagate(dist2)
+  res3.add_on_axis(mir4)
+
+  res3.compute_eigenmode()
+
+  res3.draw()
+  return res, res2, res3