7f93c08c234f1e5762f7922e82ad52bdd481408c
He Zhuang <zhuanghe199@gmail.com>
2024-02-14 15:49:22 +0100

    Integration of two structures in A3

--- A3_sketch.py
+++ A3_sketch.py
@@ -35,7 +35,8 @@ def dont():
 
 focal_length = 125
 
-beam = Beam(radius=1,angle=0)
+beam = Beam(radius=5,angle=0,wavelength=600E-6)
+beam.draw_dict["color"] = (255/256,255/256,0.0)
 Comp = Composition()
 Comp.set_light_source(beam)
 Comp.pos -= (205,0,-20)
@@ -82,3 +83,56 @@ Comp.add_on_axis(Laser_Head_out)
 Laser_Head_out.normal = -Laser_Head_out.normal
 
 Comp.draw()
+
+beam = Beam(radius=5, angle=0)
+beam.pos=[0,0,0]
+
+tele_angle1 = 8
+tele_angle2 = 3
+TFP_angle = 66
+TFP_ydist = 200
+TFP_xdist = TFP_ydist*np.tan((2*TFP_angle-90)*np.pi/180)
+TFP_dist = np.sqrt(TFP_ydist**2 + TFP_xdist**2)
+
+P1 = Mirror(phi=-90)
+P2 = Mirror(phi=90)
+PM1 = Mirror()  # pump mirror
+PM2 = Mirror()  # pump mirror 2
+M1 = Mirror(phi=-90-tele_angle1)
+M2 = Mirror(phi=-90-tele_angle2)
+M3 = Mirror(phi=90)
+M4 = Mirror(phi=90)
+R1 = Curved_Mirror(phi=-180+tele_angle1, radius=2000)
+R2 = Curved_Mirror(phi=-180+tele_angle2, radius=2500)
+TFP1 = Mirror(phi=-180+2*66)
+TFP2 = Mirror(phi=180-2*66)
+
+Setup = Composition()
+Setup.set_light_source(beam)
+
+
+
+Setup.propagate(150)
+Setup.add_on_axis(P1)
+Setup.propagate(100)
+Setup.add_on_axis(P2)
+Setup.propagate(150)
+Setup.add_on_axis(M1)
+Setup.propagate(150/np.cos(np.pi/180*tele_angle1))
+Setup.add_on_axis(R1)
+Setup.propagate(2250)
+Setup.add_on_axis(R2)
+Setup.propagate(400/np.cos(np.pi/180*tele_angle2))
+Setup.add_on_axis(M2)
+Setup.propagate(100)
+Setup.add_on_axis(M3)
+Setup.propagate(50)
+Setup.add_on_axis(TFP1)
+Setup.propagate(TFP_dist)
+Setup.add_on_axis(TFP2)
+Setup.propagate(1370)
+Setup.add_on_axis(M4)
+Setup.propagate(40)
+Setup.rotate((0,0,1), np.pi/2)
+Setup.pos += (-50,-150,20)
+Setup.draw()
