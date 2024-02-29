32b7d363de06a40ec8b210402ba395fa87b6d762
He Zhuang <zhuanghe199@gmail.com>
2024-02-29 16:35:47 +0100

    alignment_post_part fully added

--- basic_optics/element_holder.py
+++ basic_optics/element_holder.py
@@ -7,16 +7,20 @@ Created on Wed Feb 28 14:21:51 2024
 
 from .mirror import Mirror
 from .mount import Post_Marker
+from ..freecad_models.freecad_model_element_holder import Model_element_holder
 
 class Element_Marker(Post_Marker):
-  def __init__(self, element=Mirror(),size=2,**kwargs):
+  def __init__(self, aperture = 25.4,thickness=5,size=2,**kwargs):
     self.size = size
-    self.element = element
+    self.aperture = aperture
+    self.thickness = thickness
     super().__init__(**kwargs)
-    self.freecad_model 
+    self.freecad_model  = Model_element_holder()
 
   def update_draw_dict(self):
     super().update_draw_dict()
+    self.draw_dict["aperture"] = self.aperture
+    self.draw_dict["thickness"] = self.thickness
     # self.draw_dict["h1"] = self.h1
     # self.draw_dict["h2"] = (self.h1[0]+(25*self.size),self.h1[1])
     # self.draw_dict["h3"] = (self.h1[0]+(25*self.size),self.h1[1]+(25*self.size))
@@ -24,5 +28,4 @@ class Element_Marker(Post_Marker):
     # print(self.name," holes' pos=",self.h1,(self.h1[0]+(25*self.size),self.h1[1]),
     #       (self.h1[0]+(25*self.size),self.h1[1]+(25*self.size)),
     #       (self.h1[0],self.h1[1]+(25*self.size)))
-    self.draw_dict["element"] = self.element.freecad_model
     
--- freecad_models/freecad_model_element_holder.py
+++ freecad_models/freecad_model_element_holder.py
@@ -44,19 +44,89 @@ def Model_element_holder(name="marker", h1 = (0,0), h2 = (75,0), h3 = (75,75),
     obj_ele.Height = thickness
     obj_ele.Placement = Placement(Vector(0,0,0), Rotation(0,90,0), Vector(0,0,0))
     height = POS[2]
+    obj1 = DOC.addObject("Part::Box",name)
+    obj1.Label = name+" part1"
+    obj1.Length = 11
+    obj1.Width = 10
+    obj1.Height = 5
+    obj1.ViewObject.ShapeColor=color
+    obj1.Placement = Placement(Vector(-10,-5,-5), Rotation(0,0,0), Vector(0,0,0))
     obj = DOC.addObject("Part::Box",name)
     obj.Label = name
     obj.Length = 5
-    obj.Width = 5
+    obj.Width = 10
     obj.Height = height
     obj.ViewObject.ShapeColor=color
-    obj.Placement = Placement(Vector(-5,-2.5,-height), Rotation(0,0,0), Vector(0,0,0))
+    obj.Placement = Placement(Vector(-15 ,-5,-height), Rotation(0,0,0), Vector(0,0,0))
     update_geom_info(obj, geom)
+    update_geom_info(obj1, geom)
     update_geom_info(obj_ele, geom)
     part=DOC.addObject("Part::MultiFuse","Fusion")
-    part.Shapes = [obj,obj_ele]
+    part.Shapes = [obj,obj_ele,obj1]
     obj.Visibility = False
     obj_ele.Visibility = False
+    size = 1
+    NEW_POS = POS - NORMAL*12.5
+    quot_x = math.floor(NEW_POS[0]/(25*size))
+    quot_y = math.floor(NEW_POS[1]/(25*size))
+    h1 = (quot_x*(25*size),quot_y*(25*size))
+    if NEW_POS[0]-h1[0]<8:
+      h1=(h1[0]-25,h1[1])
+    if NEW_POS[1]-h1[1]<8:
+      h1=(h1[0],h1[1]-25)
+    if h1[0]+(25*size)-NEW_POS[0]<8:
+      h1=(h1[0]+25,h1[1])
+    if h1[1]+(25*size)-NEW_POS[1]<8:
+      h1=(h1[0],h1[1]+25)
+    h2 =(h1[0]+(25*size),h1[1])
+    h3 =(h1[0]+(25*size),h1[1]+(25*size))
     
+    obj_new = DOC.addObject('PartDesign::Body', name)
+    sketch = obj_new.newObject('Sketcher::SketchObject', name+'_sketch')
+    sketch.addGeometry(Part.LineSegment(Vector(h1[0]-17.5,h1[1]-7.5,0),Vector(h2[0]+7.5,h1[1]-7.5,0)),False)
+    sketch.addConstraint(Sketcher.Constraint('Horizontal',0)) 
+    sketch.addGeometry(Part.LineSegment(Vector(h2[0]+7.5,h1[1]-7.5,0),Vector(h2[0]+7.5,h3[1]+17.5,0)),False)
+    sketch.addConstraint(Sketcher.Constraint('Coincident',1,1,0,2)) 
+    sketch.addConstraint(Sketcher.Constraint('Vertical',1)) 
+    sketch.addGeometry(Part.LineSegment(Vector(h2[0]+7.5,h3[1]+17.5,0),Vector(h1[0]-17.5,h1[1]-7.5,0)),False)
+    sketch.addConstraint(Sketcher.Constraint('Coincident',2,1,1,2)) 
+    sketch.addConstraint(Sketcher.Constraint('Coincident',2,2,0,1)) 
+    pad = obj_new.newObject('PartDesign::Pad','Pad')
+    pad.Profile = sketch
+    pad.Length = 5
+    pad.ReferenceAxis = (sketch,['N_Axis'])
+    pad.Midplane = 1
+    sketch.Visibility = False
+    obj_new.Placement = Placement(Vector(0,0,7.5), Rotation(0,0,0), Vector(0,0,0))
+    obj_h1 = DOC.addObject("Part::Cone","Cone")
+    obj_h1.Radius1 = 0
+    obj_h1.Radius2 = 6
+    obj_h1.Height = 5
+    obj_h1.Placement = Placement(Vector(h1[0],h1[1],0), Rotation(0,0,0), Vector(0,0,0))
+    obj_h2 = DOC.addObject("Part::Cone","Cone")
+    obj_h2.Radius1 = 0
+    obj_h2.Radius2 = 6
+    obj_h2.Height = 5
+    obj_h2.Placement = Placement(Vector(h2[0],h2[1],0), Rotation(0,0,0), Vector(0,0,0))
+    obj_h3 = DOC.addObject("Part::Cone","Cone")
+    obj_h3.Radius1 = 0
+    obj_h3.Radius2 = 6
+    obj_h3.Height = 5
+    obj_h3.Placement = Placement(Vector(h3[0],h3[1],0), Rotation(0,0,0), Vector(0,0,0))
+    
+    holder=DOC.addObject("Part::MultiFuse","Fusion")
+    holder.Shapes = [obj_new,obj_h1,obj_h2,obj_h3]
+    obj_new.Visibility = False
+    obj_h1.Visibility = False
+    obj_h2.Visibility = False
+    obj_h3.Visibility = False
+    
+    obj_holder = DOC.addObject("Part::Cut","Cut")
+    obj_holder.Base = holder
+    obj_holder.Tool = obj
+    holder.Visibility = False
+    part_all = initialize_composition_old(name="element_holder")
+    container = part,obj_holder
+    add_to_composition(part_all, container)
     DOC.recompute()
-    return part
+    return part_all
