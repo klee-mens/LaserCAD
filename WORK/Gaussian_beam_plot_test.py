ad5d890557cf518e5d471c657e038744a0773fca
He Zhuang <zhuanghe199@gmail.com>
2024-07-15 14:46:32 +0200

    plot test

---
+++ WORK/Gaussian_beam_plot_test.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 10:46:03 2024
+
+@author: 12816
+"""
+
+import sys
+# import os
+
+pfad = __file__
+pfad = pfad.replace("\\","/") #folder conventions windows linux stuff
+pfad = pfad.lower()
+ind = pfad.rfind("lasercad")
+pfad = pfad[0:ind-1]
+if not pfad in sys.path:
+  sys.path.append(pfad)
+from copy import deepcopy
+
+from LaserCAD.basic_optics import Mirror,Beam,Cylindrical_Mirror,Grating,Ray
+from LaserCAD.basic_optics import Intersection_plane,Cylindrical_Mirror1
+from LaserCAD.basic_optics import Curved_Mirror, Composition
+
+from LaserCAD.basic_optics import Unit_Mount,Composed_Mount, Crystal, Gaussian_Beam
+from LaserCAD.non_interactings import Pockels_Cell
+from LaserCAD.non_interactings import Lambda_Plate
+
+from LaserCAD.freecad_models import clear_doc, setview, add_to_composition
+from LaserCAD.freecad_models import freecad_da
+from LaserCAD.moduls import Make_RoofTop_Mirror, Make_Periscope
+from LaserCAD.moduls.periscope import Rooftop_Mirror_Component
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+B = Gaussian_Beam()
+B.draw_gaussian_profile(center_intensity= 1.5,norm=True)
