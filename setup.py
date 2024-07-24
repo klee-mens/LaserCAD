3fc617dbca4d5430b0a5afedd869655ef95d4377
lyrahgames <lyrahgames@mailbox.org>
2024-07-24 16:46:43 +0200

    Add basic `setup.py` for `pip`

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lasercad',
+      version='1.0.0',
+      description='Attempt of OpticDesign Software that is slightly less annoying',
+      url='http://github.com/klee-mens/LaserCAD',
+      author='Clemens Anschütz',
+      author_email='clemens.anschuetz@uni-jena.de',
+      license='MIT',
+      packages=['lasercad'],
+      zip_safe=False)
