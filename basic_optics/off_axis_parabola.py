d28f9690009491f044b86d9a6e5beef85c9b80dd
klee-mens <c.anschuetz@lupotech.de>
2025-04-22 18:03:33 +0200

    Improve freecad_model_off_axis_parabola.py, off_axis_parabola.py

--- basic_optics/off_axis_parabola.py
+++ basic_optics/off_axis_parabola.py
@@ -5,35 +5,47 @@ Created on Wed Apr 16 11:43:05 2025
 
 @author: mens
 """
-from .geom_object import TOLERANCE, NORM0
+from .geom_object import TOLERANCE
+from .constants import inch
 from .ray import Ray
 from .optical_element import Opt_Element
-from .mount import Stripe_Mirror_Mount, Rooftop_Mirror_Mount, Unit_Mount
-from ..freecad_models import model_mirror, model_stripe_mirror, model_rooftop_mirror
+from .mount import Composed_Mount
+from ..freecad_models import model_off_axis_parabola
 import numpy as np
 from copy import deepcopy
 
 
 class Off_Axis_Parabola_Focus(Opt_Element):
-  def __init__(self, name="NewFocusOAP", reflected_focal_length=100, 
-               parent_focal_length=50, **kwargs):
+  def __init__(self, name="NewFocusOAP", reflected_focal_length=2*inch, angle=90, **kwargs):
     super().__init__(name=name, **kwargs)
     self.reflected_focal_length = reflected_focal_length
-    self.parent_focal_length = parent_focal_length
+    self.angle = angle #in degrees
+    self.aperture = 1*inch # Aperture in mm for drawing, Mount and clipping (not yet implemented)
+    self.thickness = 1.25*inch # Thickness in mm, importent for mount placing and drawing
+    self.freecad_model = model_off_axis_parabola
+    # example referes to https://www.thorlabs.com/thorproduct.cfm?partnumber=MPD129-M01
+    self.Mount.pos += (+self._mount_shift() - 5.5, 0, 0) #hacky but works
+    # self.Mount.pos += (+self._mount_shift() , 0, 0) #hacky but works
     
   def parent_parabola_apex(self):
     x,y,z = self.get_coordinate_system()
     rfl = self.reflected_focal_length
-    pfl = self.parent_focal_length
-    # return self.pos - y*rfl - x*pfl
-    return self.pos + y*rfl + x*pfl
+    c = self.parent_parabola_curvature()
+    sa = np.sin(self.angle/180 * np.pi)
+    return self.pos + x*c*(sa*rfl)**2 + y*sa*rfl
   
-  def _curv(self):
-    return 1 / (4 * self.parent_focal_length)
+  def parent_parabola_curvature(self):
+    ca = np.cos(self.angle/180 * np.pi)
+    sa = np.sin(self.angle/180 * np.pi)
+    rfl = self.reflected_focal_length
+    return np.sqrt( (ca/(2*sa**2*rfl))**2 + 1/(2*sa*rfl)**2 ) - ca/(2*sa**2*rfl) 
+  
+  def parent_focal_length(self):
+    return 1 / (4 * self.parent_parabola_curvature())
   
   def intersection(self, ray):
     # surface: x - ay² - az² = 0
-    curv = self._curv()
+    curv = self.parent_parabola_curvature()
     n0, y0, z0 = self.get_coordinate_system()
     n0 *= -1
     ppa = self.parent_parabola_apex()
@@ -62,9 +74,29 @@ class Off_Axis_Parabola_Focus(Opt_Element):
     ray2.pos = intersec
     n0, y0, z0 = self.get_coordinate_system()
     n0 *= -1
-    curv = self._curv()
+    curv = self.parent_parabola_curvature()
     rel = intersec - self.parent_parabola_apex()
     gradient = n0 - 2*curv*np.dot(rel,y0)*y0 - 2*curv*np.dot(rel,z0)*z0
     gradient *= 1/np.linalg.norm(gradient)
     ray2.normal += -2 * np.dot(ray.normal, gradient) * gradient
     return ray2
+  
+  def update_draw_dict(self):
+    super().update_draw_dict()
+    parentpos = self.parent_parabola_apex() - self.pos
+    self.draw_dict["parent_pos"] = parentpos[0:2]
+    self.draw_dict["parent_focal"] = self.parent_focal_length()
+    # model_off_axis_parabola(name="off_axis_parab", parent_pos=(25, 50), 
+                                # parent_focal=25, dia=25, thickness=30, 
+                                # geom=None, **kwargs):
+  def set_mount_to_default(self):
+    self.Mount = Composed_Mount(unit_model_list=["POLARIS-K1", "0.5inch_post"])
+    self.Mount.set_geom(self.get_geom())
+
+  def _mount_shift(self):
+    th = self.thickness
+    dia = self.aperture
+    c = self.parent_parabola_curvature()
+    parentpos = self.parent_parabola_apex() - self.pos
+    ppy = parentpos[1]
+    return th - c*(dia*abs(ppy) + dia**2/4)
--- freecad_models/freecad_model_off_axis_parabola.py
+++ freecad_models/freecad_model_off_axis_parabola.py
@@ -5,20 +5,95 @@ Created on Thu Apr 17 10:45:09 2025
 
 @author: mens
 """
-from .utils import freecad_da, update_geom_info, get_DOC
+from LaserCAD.freecad_models.utils import freecad_da, update_geom_info, get_DOC
+# from .utils import freecad_da, update_geom_info, get_DOC
 #import math
 
-DEFALUT_MAX_ANGULAR_OFFSET = 10
-DEFAULT_COLOR_LENS = (0/255,170/255,124/255)
-LENS_TRANSPARENCY = 50
+# DEFALUT_MAX_ANGULAR_OFFSET = 10
+DEFAULT_COLOR_OAP = (100/255,50/255,150/255)
+# LENS_TRANSPARENCY = 50
 # LENS_TRANSPARENCY = 0
 
 if freecad_da:
-  from FreeCAD import Vector
+  from FreeCAD import Vector, Rotation, Placement
+  from BOPTools import BOPFeatures
   import Part
   import Sketcher
-  from math import pi
+  # from math import pi
   
   
-def model_lens(name="lens", dia=25, Radius1=300, Radius2=0, thickness=3, geom=None, **kwargs):
-  return -1
+def model_off_axis_parabola(name="off_axis_parab", parent_pos=(25, 50), 
+                            parent_focal=25, dia=25, thickness=30, 
+                            geom=None, **kwargs):
+  
+  # parent_pos = (50, 100)
+  # parent_focal = 50
+  # thickness = 31.7
+  # dia = 25.4
+
+  parent_curvature = 1/4 / parent_focal
+  cylinder_shift = parent_curvature * (dia*abs(parent_pos[1]) + dia**2/4)
+  # cylinder_shift = parent_curvature * (abs(parent_pos[1]))
+
+  DOC = get_DOC()  
+
+  rotparab = DOC.addObject('PartDesign::Body','Body')
+  rotparab.Label = 'Body'
+  
+  sketch = rotparab.newObject('Sketcher::SketchObject','Sketch')
+  sketch.AttachmentSupport = (DOC.getObject('XY_Plane'),[''])
+  sketch.MapMode = 'FlatFace'
+  sketch.addGeometry(Part.ArcOfParabola(Part.Parabola(Vector(-22.913216,14.395430,0),Vector(25.979458,23.765106,0),Vector(0,0,1)),10.950560,53.375814),False)
+  sketch.exposeInternalGeometry(0)
+  sketch.addConstraint(Sketcher.Constraint('DistanceX',-1,1,0,3,25.979458))
+  # sketch.setDatum(2,App.Units.Quantity('22.000000 mm'))
+  sketch.setDatum(2, parent_pos[0])
+  sketch.addConstraint(Sketcher.Constraint('DistanceY',-1,1,0,3,23.831428))
+  sketch.setDatum(3, parent_pos[1])
+  sketch.addConstraint(Sketcher.Constraint('DistanceY',-1,1,1,1,18.190452))
+  sketch.setDatum(4, parent_pos[1])
+  sketch.addConstraint(Sketcher.Constraint('DistanceX',-1,1,0,1,21.193090))
+  sketch.setDatum(5, parent_pos[0])
+  sketch.addConstraint(Sketcher.Constraint('DistanceX',1,1,0,1,44.905686))
+  sketch.setDatum(6, parent_focal)
+  sketch.addConstraint(Sketcher.Constraint('DistanceX',0,2,0,1,17.816621))
+  sketch.setDatum(7, thickness + parent_pos[1] + 20)
+  sketch.addGeometry(Part.LineSegment(Vector(-67.803955,-79.955521,0),Vector(-98.153954,13.950961,0)),False)
+  sketch.addGeometry(Part.LineSegment(Vector(-98.153954,13.950961,0),Vector(-11.745703,18.949785,0)),False)
+  sketch.addConstraint(Sketcher.Constraint('Coincident',3,2,4,1)) 
+  sketch.addConstraint(Sketcher.Constraint('Coincident',4,2,0,1))
+  sketch.addConstraint(Sketcher.Constraint('Coincident',3,1,0,2))
+  sketch.addConstraint(Sketcher.Constraint('Horizontal',4))
+  sketch.addConstraint(Sketcher.Constraint('Vertical',3))
+
+  ### Begin command PartDesign_Revolution
+  revol = rotparab.newObject('PartDesign::Revolution','Revolution')
+  revol.Profile = (sketch, ['',])
+
+  revol.Angle = 360.000000
+  revol.ReferenceAxis = (sketch, ['Edge3'])
+  revol.Midplane = 0
+  revol.Reversed = 0
+  revol.Type = 0
+  revol.UpToFace = None
+
+  sketch.Visibility = False
+
+  cyl = DOC.addObject("Part::Cylinder","Cylinder")
+  cyl.Label = "Cylinder"
+  cyl.Placement=Placement(Vector(-cylinder_shift,0,0), Rotation(0,90,0), Vector(0,0,0))
+  cyl.Radius = dia / 2
+  # cyl.Height = thickness + 10
+  cyl.Height = thickness
+
+  bp = BOPFeatures.BOPFeatures(DOC)
+  offaxisparab = bp.make_cut(["Cylinder", "Body", ])
+  offaxisparab.ViewObject.ShapeColor = DEFAULT_COLOR_OAP
+  update_geom_info(offaxisparab, geom)
+  # DOC.recompute()
+  
+  return offaxisparab
+
+
+
+# oap = model_off_axis_parabola()
