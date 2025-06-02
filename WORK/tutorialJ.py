97a8788b13de35a69683925fb0813b1213e128cf
clemens <clemens.anschuetz@uni-jena.de>
2025-06-03 00:24:06 +0200

    Mod tutorialJ.py

--- WORK/tutorialJ.py
+++ WORK/tutorialJ.py
@@ -10,6 +10,9 @@ from LaserCAD.freecad_models import freecad_da, clear_doc, setview
 if freecad_da:
   clear_doc()
 
+# =============================================================================
+# Square Beam Lenses
+# =============================================================================
 import LaserCAD.basic_optics as LC
 
 sb = LC.SquareBeam(radius=5, ray_in_line=5)
@@ -23,12 +26,34 @@ kt.add_on_axis(lens1)
 kt.propagate(100+200)
 kt.add_on_axis(lens2)
 kt.propagate(200)
-kt.draw()
+kt.pos = (10, 20, 120)
+# kt.draw()
+
+# =============================================================================
+# Anastigmatic Mirror Telescope
+# =============================================================================
+
+mir1 = LC.Curved_Mirror(radius=250, phi=180-8)
+mir2 = LC.Curved_Mirror(radius=250, phi=0,
+                        theta=180-8)
+mir2.set_mount(LC.Composed_Mount(
+  unit_model_list=["KS1", "0.5inch_post"]))
+
+mt = LC.Composition()
+mt.set_light_source(LC.Beam(radius=2))
+mt.propagate(350)
+mt.add_on_axis(mir1)
+mt.propagate(250)
+mt.add_on_axis(mir2)
+mt.propagate(350)
+mt.draw()
 
 
 # =============================================================================
 # draw section
 # =============================================================================
+kt.draw()
+# mt.draw()
 
 
 if freecad_da:
