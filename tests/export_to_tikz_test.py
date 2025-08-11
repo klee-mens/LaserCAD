245de8d922fa0f0ce58a954d752bf1e441b5ab61
klee-mens <c.anschuetz@lupotech.de>
2025-08-11 15:47:28 +0200

    Comment a line in tests/export_to_tikz_test.py

--- tests/export_to_tikz_test.py
+++ tests/export_to_tikz_test.py
@@ -3,8 +3,8 @@ from LaserCAD.basic_optics import export_to_TikZ
 
 teles = Make_Telescope()
 
-import os
-Folder = os.path.dirname(os.path.abspath(__file__))
+# import os
+# Folder = os.path.dirname(os.path.abspath(__file__))
 
 export_to_TikZ(teles)
 # export_to_TikZ(teles, filename=os.path.join(Folder, "telescope.tex"))
