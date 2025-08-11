bf450d5dd892c9a60426571e39d24b4755facf28
klee-mens <c.anschuetz@lupotech.de>
2025-08-11 15:06:25 +0200

    Delete GUI, v1.1.0

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="LaserCAD",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(include=["LaserCAD", "LaserCAD.*"]),
 )
