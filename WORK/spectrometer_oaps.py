befce0d78471c7f1c3178a77984d60ce0b79f9be
clemens <clemens.anschuetz@uni-jena.de>
2025-09-28 23:11:52 +0200

    Improve vac spectro

--- WORK/spectrometer_oaps.py
+++ WORK/spectrometer_oaps.py
@@ -8,12 +8,14 @@ Created on Thu May  8 09:15:24 2025
 
 from LaserCAD.basic_optics import Beam, Lens, Off_Axis_Parabola, Mirror, Composition, inch, Geom_Object
 from LaserCAD.non_interactings import Crystal
+from LaserCAD.basic_optics import ThickBeamsplitter, Component, Composed_Mount
 from LaserCAD.freecad_models import freecad_da, clear_doc, setview
 from LaserCAD.freecad_models.utils import thisfolder, load_STL
+from LaserCAD.basic_optics.mount import Stages_Mount
 import numpy as np
 
 
-
+COOLING_ROD_SHIFT = 80
 
 cooling_rod_file = thisfolder+"misc_meshes/Cooling_Rod.stl"
 chamber_file = thisfolder+"misc_meshes/Vacuum_Chamber.stl"
@@ -22,7 +24,7 @@ cool_rod = Geom_Object()
 cool_rod.freecad_model = load_STL
 cool_rod.draw_dict["stl_file"]=cooling_rod_file
 cool_rod.draw_dict["color"] = (0.6, 0.2, 0.1)
-cool_rod.pos = (0,0,0)
+cool_rod.pos = (0,COOLING_ROD_SHIFT,0)
 
 
 chamber = thisfolder+"misc_meshes/Cooling_Rod.stl"
@@ -57,11 +59,30 @@ oap_focus = 2*inch
 oap_colim = Off_Axis_Parabola(colim=True, reflected_focal_length=oap_focus, name="ColimOAP")
 oap_fibre = Off_Axis_Parabola(colim=False, reflected_focal_length=oap_focus, name="FocusOAP", theta=180)
 
-crystal = Crystal(width=6, height=6, thickness=2, name="Sample")
+thickness_crystal = 5
+refractive_index_crystal = 1.3
+delta = thickness_crystal * refractive_index_crystal
+aperture_crystal = 12
+crystal = Crystal(width=6, height=6, thickness=thickness_crystal, name="Sample")
+crystal = ThickBeamsplitter(angle_of_incidence=0, thickness=thickness_crystal, 
+                            refractive_index=refractive_index_crystal)
+crystal.aperture = 12
+crystal.Mount.invisible = True
+# from LaserCAD.moduls.transmission_disk import Transmission_Disk
+
+# crystal = Transmission_Disk(name="YbCaF2", refractive_index=refractive_index_crystal,
+                            # AOI=0, thickness=thickness_crystal, aperture=aperture_crystal)
+# crystal.non_opticals[0].Mount.invisible = True
+
 
 spectro = Composition(name="Spectro")
 spectro.set_light_source(fiber1)
-spectro.propagate(white_light_focus*1)
+
+fibre_holder = Component()
+fibre_holder.set_mount(Stages_Mount(basic_mount=Composed_Mount(["KS1", "1inch_post"])))
+spectro.add_on_axis(fibre_holder)
+
+spectro.propagate(white_light_focus*1-delta)
 spectro.add_on_axis(teles_lens1)
 spectro.propagate(white_light_focus*1)
 spectro.add_on_axis(teles_mirror)
@@ -70,13 +91,15 @@ spectro.add_on_axis(teles_lens2)
 spectro.propagate(white_light_focus*1)
 spectro.add_on_axis(crystal)
 spectro.propagate(oap_focus)
+# spectro.add_supcomposition_on_axis(crystal)
+# spectro.propagate(oap_focus)
 spectro.add_on_axis(oap_colim)
 spectro.propagate(180)
 spectro.add_on_axis(oap_fibre)
 spectro.propagate(oap_focus)
 
 spectro.rotate(vec=(0,0,1), phi=np.pi/2)
-spectro.pos = (200, 57-210, 110)
+spectro.pos = (200, 57-210 + COOLING_ROD_SHIFT, 110)
 # spectro.pos = crystal.pos
 
 # =============================================================================
@@ -95,19 +118,24 @@ pump_teles_lens2.aperture = 2*inch
 pump_teles_mirror = Mirror(phi=-95)
 pump_teles_mirror.aperture = 2*inch
 
+pump_geom = spectro.compute_beams()[4].get_geom()
+
 pump_comp = Composition(name="PumpLine")
 pump_comp.set_light_source(pump_fibre)
-pump_comp.set_geom(crystal.get_geom())
+pump_comp.set_geom(pump_geom)
 pump_comp.rotate(vec=(0,0,1), phi=30*np.pi/180)
 pump_comp.propagate(pump_focal)
 pump_comp.add_on_axis(pump_teles_lens2)
-pump_comp.propagate(pump_focal)
+pump_comp.propagate(pump_focal*1.5)
 pump_comp.add_on_axis(pump_teles_mirror)
-pump_comp.propagate(pump_focal)
+pump_comp.propagate(pump_focal*0.5)
 pump_comp.add_on_axis(pump_teles_lens1)
 pump_comp.propagate(pump_focal)
 
+fibre_holder_pump = Component()
+fibre_holder_pump.set_mount(Stages_Mount(basic_mount=Composed_Mount(["KS1", "1inch_post"])))
 
+pump_comp.add_on_axis(fibre_holder_pump)
 
 # =============================================================================
 # draw selesction
